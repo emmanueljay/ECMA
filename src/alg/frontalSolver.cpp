222a523a2fc4de9615d2cef26bd20f50bc1fc111
Maxime Dufour <dufour@ensta.fr>
2016-01-24 18:04:02 +0100

    working on frontal

--- src/alg/frontalSolver.cpp
+++ src/alg/frontalSolver.cpp
@@ -8,9 +8,30 @@ using namespace std;
 #include <ilcplex/ilocplex.h>
 #include <ilcp/cp.h>
 
+typedef IloArray<IloBoolVarArray> BoolVarMatrix;
+typedef IloArray<IloIntVarArray>  IntVarMatrix;
+
 bool FrontalSolver::solve() {
   LOG(INFO) << name_ << " :: " << description_; 
   LOG(INFO) << data_.n;
   LOG(INFO) << "We did not find a solution !";
+  
+  IloEnv env;
+  IloModel model = IloModel (env);
+  IloCplex cplex = IloCplex(model);
+  int n = data_.n;
+  int m = data_.m;
+
+  BoolVarMatrix x_ij(env);
+
+
+
+
+
+
+
+
+
+
   return false;
 }
