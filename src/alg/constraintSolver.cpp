f48f2a013b4dacbb500d2b0e3feb832ebe8989ea
Emmanuel Jay <emmanuel@classnco.com>
2016-01-29 15:50:10 +0100

    Constraint solver

--- src/alg/constraintSolver.cpp
+++ src/alg/constraintSolver.cpp
@@ -37,7 +37,50 @@ bool ConstraintSolver::solve() {
         model.add(x[i][j] == 0);
     
     VLOG(3) << "Constraint 2. The ratio should be greater than 2";
-    
+
+    IloExpr sum_x(env,0);
+    IloExpr sum_HCp(env,0);
+    IloExpr sum_Cp(env,0);
+    IloExpr sum_HCa(env,0);
+    IloExpr sum_Ca(env,0);
+
+    for (int i = 0; i < data_.m; ++i)
+    for (int j = 0; j < data_.n; ++j) {
+      sum_x += x[i][j];
+      sum_HCp += data_.Hp[i][j]*data_.Cp[i][j]*x[i][j];
+      sum_Cp  += data_.Cp[i][j]*x[i][j];
+      sum_HCa += data_.Ha[i][j]*data_.Ca[i][j]*x[i][j];
+      sum_Ca  += data_.Ca[i][j]*x[i][j];
+    }
+  
+    model.add(sum_HCp*sum_Ca + sum_HCa*sum_Cp >= 2*sum_Cp*sum_Ca);
+
+    VLOG(3) << "Constraint 3. Connexity";
+
+    for (int i = 0; i < data_.m; ++i)
+    for (int j = 0; j < data_.n; ++j) {
+      if (i==0) {
+        if (j==0)  model.add((x[i][j] == 0) ||  ((x[i+1][j] == 1) || (x[i][j+1] == 1)));
+        else if (j==(data_.n-1)) model.add((x[i][j] == 0) || ((x[i+1][j] == 1) || (x[i][j-1] == 1)));
+        else model.add((x[i][j] == 0) || ((x[i+1][j] == 1) || (x[i][j-1] == 1) || (x[i][j+1] == 1)));
+      }
+      else if (i==(data_.m-1)) {
+        if (j==0)  model.add((x[i][j] == 0) ||  ((x[i-1][j] == 1) || (x[i][j+1] == 1)));
+        else if (j==(data_.n-1)) model.add((x[i][j] == 0) || ((x[i-1][j] == 1) || (x[i][j-1] == 1)));
+        else model.add((x[i][j] == 0) || ((x[i-1][j] == 1) || (x[i][j-1] == 1) || (x[i][j+1] == 1)));
+      }
+      else {
+        if (j==0)  model.add((x[i][j] == 0) ||  ((x[i+1][j] == 1) || (x[i-1][j] == 1) || (x[i][j+1] == 1)));
+        else if (j==(data_.n-1)) model.add((x[i][j] == 0) || ((x[i+1][j] == 1) || (x[i-1][j] == 1) || (x[i][j-1] == 1)));
+        else model.add((x[i][j] == 0) || ((x[i+1][j] == 1) || (x[i-1][j] == 1) || (x[i][j-1] == 1) || (x[i][j+1] == 1)));      
+      }
+    }
+
+    VLOG(3) << "Constraint 4. Objective value";
+    // model.add(sum_x >= 5);
+
+    IloObjective objective = IloMaximize(env,sum_x);
+    model.add(objective);
 
     VLOG(1) << "Solving model using constraint programming";
     IloCP cp(model);
