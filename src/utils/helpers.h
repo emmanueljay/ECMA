728371408f8778ea7f95a5727bef5998bcedf1d9
Emmanuel Jay <emmanuel@classnco.com>
2016-02-08 19:29:54 +0100

    Max display

--- src/utils/helpers.h
+++ src/utils/helpers.h
@@ -19,6 +19,8 @@ std::string to_string(int num);
 // Function that pretty print matrix
 void print_table(const std::vector<std::vector<double> >& table, double max);
 
+// Return max of a matrix
+double max_mat(const std::vector<std::vector<double> >& table);
 
 } // namespace helpers
 } // namespace ecma
